f16b969719a6dbbba68cc54648da76f6619063fa
victormc13 <viktorsb14@gmail.com>
2023-04-27 16:05:30 -0500

    Fix script bugs

--- scripts/script.js
+++ scripts/script.js
@@ -68,41 +68,54 @@ const speakersData = [
 
 function renderSpeakersSection() {
   const speakersSection = document.getElementById('speakers');
-  speakersSection.classList.add('speakers-section', 'p-3', 'p-lg-5', 'h-100', 'bg-white');
+
+  if (speakersSection) {
+    speakersSection.classList.add(
+      'speakers-section',
+      'p-3',
+      'p-lg-5',
+      'h-100',
+      'bg-white',
+    );
+  }
 
   const speakersContent = `<h2 class="text-center">Featured Speakers</h2>
   <div class="guide-bar"></div>
   <div class="articles-container"></div>`;
 
-  if (speakersSection.nodeType === Node.ELEMENT_NODE) {
-    speakersSection.insertAdjacentHTML('afterbegin', speakersContent);
+  if (speakersSection) {
+    if (speakersSection.nodeType === Node.ELEMENT_NODE) {
+      speakersSection.insertAdjacentHTML('afterbegin', speakersContent);
+    }
   }
 
   const articlesContainer = document.querySelector('.articles-container');
 
-  if (articlesContainer.nodeType === Node.ELEMENT_NODE) {
-    speakersData.forEach((speaker) => articlesContainer.insertAdjacentHTML(
-      'afterbegin',
-      `<article class="d-flex gap-2">
-    <img
-      class="p-0"
-      src="${speaker.img}"
-      alt="speaker photo"
-      width="150px"
-      height="130px"
-    />
-    <div class="d-flex flex-column speaker gap-2">
-      <h4 class="fw-bold m-0">${speaker.name}</h4>
-      <p class="m-0">
-        ${speaker.review}
-      </p>
-      <div class="little-guide-bar m-0"></div>
-      <p class="fw-bold">
-        ${speaker.description}
-      </p>
-    </div>
-  </article>`,
-    ));
+  if (articlesContainer) {
+    if (articlesContainer.nodeType === Node.ELEMENT_NODE) {
+      speakersData.forEach((speaker) => articlesContainer.insertAdjacentHTML(
+        'afterbegin',
+        `<article class="d-flex gap-2">
+      <img
+        class="p-0"
+        src="${speaker.img}"
+        alt="speaker photo"
+        width="150px"
+        height="130px"
+      />
+      <div class="d-flex flex-column speaker gap-2">
+        <h4 class="fw-bold m-0">${speaker.name}</h4>
+        <p class="m-0">
+          ${speaker.review}
+        </p>
+        <div class="little-guide-bar m-0"></div>
+        <p class="fw-bold">
+          ${speaker.description}
+        </p>
+      </div>
+    </article>`,
+      ));
+    }
   }
 }
 
